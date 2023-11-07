1922690d520a513e7b6ea1b475d14089f79b85e8
hoon <jiroh618@gmail.com>
2023-11-08 01:32:52 +0900

    feat: Add style class for model

--- django/musinsa_trend/outfits/models.py
+++ django/musinsa_trend/outfits/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Style(models.Model):
+    subject = models.CharField(max_length=64)
+    date = models.DateField() # datetime->date 
+    category = models.CharField(max_length=16)
+    views = models.IntegerField(default=0)
+    URL = models.TextField(null=True)
+    tag = models.TextField(null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    deleted_at = models.DateTimeField()
+    updated_at = models.DateTimeField(auto_now=True)
