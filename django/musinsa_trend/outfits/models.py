5659529f8fead9c0f3acfb6a0561a48bb4bef33a
hoon <jiroh618@gmail.com>
2023-11-08 15:47:52 +0900

    feat: Add rest of class for model
    - Goods , Style의  id값은 자동추가라서 넣지 않았습니다.
    
    Related: #7

--- django/musinsa_trend/outfits/models.py
+++ django/musinsa_trend/outfits/models.py
@@ -2,11 +2,34 @@ from django.db import models
 
 class Style(models.Model):
     subject = models.CharField(max_length=64)
-    date = models.DateField() # datetime->date 
+    date = models.DateField(null=False)
     category = models.CharField(max_length=16)
     views = models.IntegerField(default=0)
     URL = models.TextField(null=True)
     tag = models.TextField(null=True)
     created_at = models.DateTimeField(auto_now_add=True)
-    deleted_at = models.DateTimeField()
+    deleted_at = models.DateTimeField(null=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'tb_style'
+
+class Goods(models.Model):
+    name = models.CharField(max_length=64, null=False)
+    brand = models.CharField(max_length=64, null=False)
+    origin_price = models.IntegerField(null=False)
+    discounted_price = models.IntegerField(null=True)
+    created_at = models.DateTimeField(null=False)
+    deleted_at = models.DateTimeField(null=True)
+    updated_at = models.DateTimeField(null=True)
+
+    class Meta:
+        db_table = 'tb_goods'
+
+class StyleGoods(models.Model):
+    id = models.AutoField(primary_key=True)  
+    goods = models.ForeignKey(Goods, on_delete=models.CASCADE)  
+    style = models.ForeignKey(Style, on_delete=models.CASCADE)  
+
+    class Meta:
+        db_table = 'tb_style_goods'
