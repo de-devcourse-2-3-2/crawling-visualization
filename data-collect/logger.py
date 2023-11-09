ebeb59021bf9dfece066197d553dbc02572fafbb
Jaeho-99 <wogh6860@gmail.com>
2023-11-09 20:31:32 +0900

    fix: add findCaller() func.

--- data-collect/logger.py
+++ data-collect/logger.py
@@ -1,14 +1,16 @@
 
 import logging
-
+import datetime
 
 # Configure the root logger
 def setLogOptions():
+    now = datetime.datetime.now() # 프로그램 실행 시각 저장
+    caller_name = logging.Logger.findCaller(self=debug)[2] # 호출자 정보 추출. (파일 이름, 행 번호, 함수 이름, 스택 정보)의 튜플 형식으로 반환됨.
     logging.basicConfig(
         level=logging.DEBUG,
         format='%(asctime)s - %(levelname)s - %(message)s',
         datefmt='%Y-%m-%d %H:%M:%S',
-        filename='app.log',  # Specify the file name for file-based logging (optional)
+        filename=f'.\\log\\{caller_name}_{now.date()}_{now.hour}h_{now.minute}m.log',  # Specify the file name for file-based logging (optional)
     )
 
     
@@ -20,3 +22,7 @@ def setLogOptions():
 
     # Attach the console handler to the root logger
     logging.getLogger('').addHandler(console_handler)
+
+def debug(data):
+    setLogOptions()
+    logging.debug(data)
