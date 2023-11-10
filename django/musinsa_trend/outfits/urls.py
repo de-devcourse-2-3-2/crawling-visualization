121e55d1348c4c27fb786bc14ac3774b2fdc2b2b
bongseung <cbs02112@gmail.com>
2023-11-10 17:59:06 +0900

    fix : show img
     - operates with sample image

--- django/musinsa_trend/outfits/urls.py
+++ django/musinsa_trend/outfits/urls.py
@@ -1,22 +1,26 @@
+import os
 from django.urls import path
 from django.views.generic import TemplateView
 from rest_framework import permissions
 from rest_framework.schemas import get_schema_view
 from . import views
 
+# static file url example :
+# www.example.com:8000/static/media/image01.png
+
 urlpatterns = [
-    path('index', views.index, name='index'),
-    path('chart/', views.chart, {'chart_type': 0}, name='chart'),
-    path('style/', views.style, {'by': 'season'}, name='style'),
+    path('index/', views.index, name='index'),
+    path('chart', views.chart, {'chart_type' : 0}, name='chart'),
+    path('style', views.style, {'by': 'season'}, name='style'),
     
-    # Schema view of app
+    # Generic schema view of app
     path('schema_view/', get_schema_view(
         title="Musinsa Trend Analyzer",
         description="All APIs",
         version="1.0.0"
     ), name='schema_view'),
 
-    # Openapi schema view UI
+    # Openapi schema view with UI
     path('openapi/', TemplateView.as_view(
         template_name='openapi_schema.html',
         extra_context={'schema_url':'schema_view'}
--- django/musinsa_trend/outfits/views.py
+++ django/musinsa_trend/outfits/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from plot import plot
@@ -7,16 +8,14 @@ from plot import plot
 
 @api_view(['GET'])
 def index(request) :
-    context = chart(request, 1)
-    return render(request,'index.html', context)
+    context = {'img_url' : 'imgage03.png'}
+    return render(request,'chart_with_js.html', context)
 
 @api_view(['GET'])
-def chart(request, chart_type):
-    data = {'img_url' : "chart/image01.png"}
-    if chart_type == 1 :
-        #data = plot.chart()
-        pass
-    return Response(data)
+def chart(request,chart_type):
+    # plot.get_img()
+    context = {'img_url' : 'image03.png'}
+    return Response(context)
 
 @api_view(['GET'])
 def style(request, by):
