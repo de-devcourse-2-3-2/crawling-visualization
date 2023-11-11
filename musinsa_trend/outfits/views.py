b001c8d9121e5954f7bcba44d8e49a4d2a9c4fd5
jiroh1 <jiroh618@gmail.com>
2023-11-12 00:18:53 +0900

    feat: Add function for searching popular styles by category
    - JsonResponse로 return 하도록 함수 구현
    
    Resolved: #18

--- musinsa_trend/outfits/views.py
+++ musinsa_trend/outfits/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from django.db.models import Count
 from .models import Style, StyleGoods, Goods
+from django.http import JsonResponse
+
+import logging
+from logger import setLogOptions
+
+setLogOptions()
+logger = logging.getLogger(__name__)
 
 def style_brand_count(request):
     styles = Style.objects.all()
@@ -12,6 +19,7 @@ def style_brand_count(request):
         goods_ids = StyleGoods.objects.filter(style=selected_style).values_list('goods_id', flat=True)
         brand_count = Goods.objects.filter(id__in=goods_ids).values('brand').annotate(total=Count('brand'))
 
+    logger.info(f'*******{request}, style : {styles}, brand_count : {brand_count}')
     return render(request, 'test.html', {
         'styles': styles,
         'brand_count': brand_count
@@ -38,8 +46,31 @@ def season_style_trend(request):
         category__in=[style['category'] for style in styles]
     ).count()
 
+    logger.info(f'*******{request}, style : {styles}, brand_count : {other_count}')
     return render(request, 'test2.html', {
         'styles': styles,
         'other_count': other_count,
         'season': season
     })
+
+def popular_styles_by_category(request):
+    """
+    카테고리에 대한 최상위 스타일 5개 조회
+    해당 스타일에 있는 사진, 그 스타일에 있는 상품 list 조회
+    """
+    category_name = request.GET.get('category')
+    if category_name:
+        top_styles = Style.objects.filter(category=category_name).order_by('-views')[:5]
+
+        results = [
+            {
+                'subject': style.subject,
+                'views': style.views,
+                'url': style.URL,
+                'goods_list': list(StyleGoods.objects.filter(style=style).values('goods__name', 'goods__brand'))
+            } for style in top_styles
+        ]
+        logger.info(f'*******popular_styles_by_category : {results} 이 검색되었습니다.')
+        return JsonResponse(results, safe=False)
+    else:
+        return JsonResponse({'error': '카테고리가 지정되지 않았습니다.'}, status=400)
