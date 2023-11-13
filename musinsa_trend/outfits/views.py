ff61e71835b7a996df8d0eddd7ec719d9955bcfa
cbs02112 <cbs02112@gmail.com>
2023-11-13 21:29:56 +0900

    style : minor change

--- musinsa_trend/outfits/views.py
+++ musinsa_trend/outfits/views.py
@@ -2,13 +2,15 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from plot import plot
+from plot.plot import Plot
+from plot.utils import Utils
 
 # Create your views here.
-# def style_brand_count(style_id):
+
+# style_brand_count():
 #     return Json
 
-# def season_style_trend(season):
+# season_style_trend(season):
 #     return Json
 
 
@@ -24,11 +26,13 @@ def chart(request,chart_type):
         # Plot.line(data)
         filename_img = Plot.FILE_NAME_LINE
     elif chart_type == 1 : # 스타일 카테고리 별 브랜드 점유
-        data = category_brand_count()
+        raw_data = category_brand_count()
+        data = Utils.get_data_for_pie(raw_data)
         Plot.pie(data)
         filename_img = Plot.FILE_NAME_PIE
     elif chart_type == 2 : # 시즌 별 스타일 stacked bar chart
-        data = season_style_trend()
+        raw_data = season_style_trend()
+        data = Utils.get_data_for_stacked_bar(raw_data)
         Plot.stacked_bar(data)
         filename_img = Plot.FILE_NAME_STACKED_BAR
     return Response({'filename_img' : filename_img})
