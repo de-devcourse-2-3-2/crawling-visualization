242bae7955e116753e5c128b5fd6ef39f9456316
Jaeho-99 <wogh6860@gmail.com>
2023-11-14 13:39:33 +0900

    fix: add style_goods_table code

--- crawling/main/database/csv_to_postgres.py
+++ crawling/main/database/csv_to_postgres.py
@@ -3,10 +3,31 @@
 # postgres_write.py 파일의 return_postgresql_conn 메서드에서 db_config에서 encoding방식 utf16으로 일단 적용.
 import csv_postgres_write as cpw
 import pandas as pd
-import numpy as np
+
+def get_season(date):
+    '''
+    월(날짜)에 따라 계절명 반환 함수.
+    input: "YYYY-MM-DD"
+    ouput: {spring, summer, fall, winter} 중 하나
+    2~4 : spring
+    5~8 : summer
+    9~10 : fall
+    11~1 : winter
+    '''
+    month = int(date.split('-')[1])
+    if 2 <= month <= 4:
+        return "spring"
+    elif 5 <= month <= 8:
+        return "summer"
+    elif 9 <= month <= 10:
+        return "fall"
+    else:
+        return "winter"
+
 
 def main():
-    cpw.tables_create()
+    CPW = cpw.DB_Write()
+    CPW.tables_create()
 
     codishop_dataset = pd.read_csv('codishop(2023-11-09_12h_2m).csv', encoding='utf-16')
     codimap_dataset = pd.read_csv('codimap(2023-11-10_10h_19m).csv', encoding='utf-16')
@@ -19,6 +40,7 @@ def main():
         date = row['date'] # date: 게시된 날짜. ex)"2023-11-07"(string)
         category = row['category'] # category: 스타일 카테고리. ex)"댄디"(string)
         views = int(row['views']) # views: 조회수. ex)1100(int)
+        season = get_season(date)
         img_url = row['URL'] # URL: 이미지 URL. ex)"https://.*"(string)
         tags = row['tag'] # tags: 태그 목록. ex)["#겨울", "#깔끔한", ..](list)
         tags = tags.replace('[','').replace(']','').replace(',','').replace("'",'').replace(' ','')
@@ -31,8 +53,8 @@ def main():
         # print(type(tags))
 
         # Insert
-        style_dataset = [(subject, date, category, views, img_url, tags),]
-        cpw.insert_style_data(style_list=style_dataset) # subject, date, category, views, url, tag, created_at, updated_at, deleted_at
+        style_dataset = (subject, date, category, views, season, img_url, tags)
+        style_id = CPW.insert_style_data(style_data=style_dataset)
 
         # goods table 관련 정보들
         brands = row['brands'].replace('[','').replace(']','').replace("'",'').split(',') # brands: 브랜드 목록. ex)["나이키", "아디다스", ..](list)
@@ -50,10 +72,11 @@ def main():
                 # print(type(brand))
                 # print(type(price))
                 # print(type(del_price))
-                goods_dataset = [(name, brand, price, del_price),]
+                goods_dataset = (name, brand, price, del_price)
 
                 # 상품 테이블 insert
-                cpw.insert_goods_data(goods_list=goods_dataset)
+                goods_id = CPW.insert_goods_data(goods_data=goods_dataset)
+                CPW.insert_style_goods(style_id=style_id, goods_id=goods_id)
 
 if __name__ == '__main__':
     main()
