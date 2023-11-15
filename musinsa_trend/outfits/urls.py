48d4487727f86ad1bfd35df5b9e3b87c37a3a95f
jiroh1 <jiroh618@gmail.com>
2023-11-15 21:15:18 +0900

    test: visualization

--- musinsa_trend/outfits/urls.py
+++ musinsa_trend/outfits/urls.py
@@ -9,23 +9,24 @@ from . import views
 # www.example.com:8000/static/media/image01.png
 
 urlpatterns = [
-    path('index/', views.index, name='index'),
-    path('chart', views.chart, name='chart'),
-    path('styles_by_category', views.stylecat, name='styles_by_season'),
-    path('styles_by_season', views.stylesea, name='styles_by_season'),
+    # path('index/', views.index, name='index'),
+    # path('chart', views.chart, name='chart'),
+    # path('styles_by_category', views.stylecat, name='styles_by_season'),
+    # path('styles_by_season', views.stylesea, name='styles_by_season'),
     
-    # Generic schema view of app
-    path('schema_view/', get_schema_view(
-        title="Musinsa Trend Analyzer",
-        description="All APIs",
-        version="1.0.0"
-    ), name='schema_view'),
+    # # Generic schema view of app
+    # path('schema_view/', get_schema_view(
+    #     title="Musinsa Trend Analyzer",
+    #     description="All APIs",
+    #     version="1.0.0"
+    # ), name='schema_view'),
 
-    # Openapi schema view with UI
-    path('openapi/', TemplateView.as_view(
-        template_name='openapi_schema.html',
-        extra_context={'schema_url':'schema_view'}
-    ), name='openapi_schema_view'),
+    # # Openapi schema view with UI
+    # path('openapi/', TemplateView.as_view(
+    #     template_name='openapi_schema.html',
+    #     extra_context={'schema_url':'schema_view'}
+    # ), name='openapi_schema_view'),
+    path('top-styles/<str:season>/', views.top_styles_by_season, name='top_styles_by_season'),
 ]
 
 # urlpatterns = [
--- musinsa_trend/outfits/views.py
+++ musinsa_trend/outfits/views.py
@@ -12,160 +12,178 @@ from logger import setLogOptions
 setLogOptions()
 logger = logging.getLogger(__name__)
 
-# Create your views here.
+# # Create your views here.
 
-# style_brand_count():
-#     return Json
+# # style_brand_count():
+# #     return Json
 
-# season_style_trend(season):
-#     return Json
+# # season_style_trend(season):
+# #     return Json
 
 
-@api_view(['GET'])
-def index(request) :
-    return render(request,'index.html')
+# @api_view(['GET'])
+# def index(request) :
+#     return render(request,'index.html')
 
-@api_view(['GET'])
-def chart(request):
-    filename_img = ''
-    msg = ''
-    plot = Plot()
-    utils = Utils()
-    chart_type = request.GET.get('chart_type', 1)
-    category = request.GET.get('category', '스포티')
-    if chart_type == '1' : # 스타일 동향 line chart
-        # 로직이 구현되지 않음
-        # data = some_function()
-        # Plot.line(data)
-        filename_img = Plot.FILE_NAME_LINE
-    elif chart_type == '2' : # 스타일 카테고리 별 브랜드 점유 Pie chart
-        raw_data = category_brand_count()
-        # Or you use sample data with alternative code below
-        # raw_data = Utils.SAMPLE_PIE
-        data = Utils.get_data_for_pie(category,raw_data)
-        if plot.pie(data) :
-            filename_img = Plot.FILE_NAME_PIE
-    elif chart_type == '3' : # 시즌 별 스타일 stacked bar chart
-        raw_data = season_style_trend()
-        # print('-'*50)
-        # print(raw_data)
-        # print('-'*50)
-        # Or you use sample data with alternative code below
-        # raw_data = Utils.SAMPLE_STACKED_BAR
-        data = utils.get_data_for_stacked_bar(raw_data)
-        if plot.stacked_bar(data) :
-            filename_img = plot.FILE_NAME_STACKED_BAR
-    return Response({'filename_img' : filename_img})
+# @api_view(['GET'])
+# def chart(request):
+#     filename_img = ''
+#     msg = ''
+#     plot = Plot()
+#     utils = Utils()
+#     chart_type = request.GET.get('chart_type', 1)
+#     category = request.GET.get('category', '스포티')
+#     if chart_type == '1' : # 스타일 동향 line chart
+#         # 로직이 구현되지 않음
+#         # data = some_function()
+#         # Plot.line(data)
+#         filename_img = Plot.FILE_NAME_LINE
+#     elif chart_type == '2' : # 스타일 카테고리 별 브랜드 점유 Pie chart
+#         raw_data = category_brand_count()
+#         # Or you use sample data with alternative code below
+#         # raw_data = Utils.SAMPLE_PIE
+#         data = Utils.get_data_for_pie(category,raw_data)
+#         if plot.pie(data) :
+#             filename_img = Plot.FILE_NAME_PIE
+#     elif chart_type == '3' : # 시즌 별 스타일 stacked bar chart
+#         raw_data = season_style_trend()
+#         # print('-'*50)
+#         # print(raw_data)
+#         # print('-'*50)
+#         # Or you use sample data with alternative code below
+#         # raw_data = Utils.SAMPLE_STACKED_BAR
+#         data = utils.get_data_for_stacked_bar(raw_data)
+#         if plot.stacked_bar(data) :
+#             filename_img = plot.FILE_NAME_STACKED_BAR
+#     return Response({'filename_img' : filename_img})
 
-@api_view(['GET'])
-def stylecat(request):
-    data = popular_styles_by_category(request)
-    return render(request, 'style_list.html', data)
+# @api_view(['GET'])
+# def stylecat(request):
+#     data = popular_styles_by_category(request)
+#     return render(request, 'style_list.html', data)
 
-@api_view(['GET'])
-def stylesea(request) :
-    data = top_styles_by_season(request)
-    return render(request, 'style_list.html', data)
+# @api_view(['GET'])
+# def stylesea(request) :
+#     data = top_styles_by_season(request)
+#     return render(request, 'style_list.html', data)
 
-def category_brand_count():
-    """
-    스타일 선택 (ex . 아메카지)
-    모든 브랜드 개수 확인
-    """
+# def category_brand_count():
+#     """
+#     스타일 선택 (ex . 아메카지)
+#     모든 브랜드 개수 확인
+#     """
     
-    category_count_data = []
-    for category in Style.objects.values('category').distinct():
-        category_name = category['category']
-        goods_ids = StyleGoods.objects.filter(style__category=category_name).values_list('goods_id', flat=True)
-        brand_count = Goods.objects.filter(id__in=goods_ids).values('brand').annotate(total=Count('brand'))
-        category_count_data.append({
-            'category': category_name,
-            'brand_counts': list(brand_count)
-        })
+#     category_count_data = []
+#     for category in Style.objects.values('category').distinct():
+#         category_name = category['category']
+#         goods_ids = StyleGoods.objects.filter(style__category=category_name).values_list('goods_id', flat=True)
+#         brand_count = Goods.objects.filter(id__in=goods_ids).values('brand').annotate(total=Count('brand'))
+#         category_count_data.append({
+#             'category': category_name,
+#             'brand_counts': list(brand_count)
+#         })
 
-    return {
-        'category_brand_counts': category_count_data
-    }
+#     return {
+#         'category_brand_counts': category_count_data
+#     }
 
-def season_style_trend():
-    """
-    계절 별 조회수 10,000개 이상인 스타일에 대해서 카테고리별 개수 확인
-    나타나는 값: 상위 5개 카테고리
-    나머지에 대해서는 합해서 반환
-    """
-    # request 없이 계절 모두 구현 되도록 변경 
-    seasons = ['spring', 'summer', 'fall', 'winter']
-    response_data = {'spring': {} , 'summer' : {}, 'fall' : {}, 'winter' : {}}
+# def season_style_trend():
+#     """
+#     계절 별 조회수 10,000개 이상인 스타일에 대해서 카테고리별 개수 확인
+#     나타나는 값: 상위 5개 카테고리
+#     나머지에 대해서는 합해서 반환
+#     """
+#     # request 없이 계절 모두 구현 되도록 변경 
+#     seasons = ['spring', 'summer', 'fall', 'winter']
+#     response_data = {'spring': {} , 'summer' : {}, 'fall' : {}, 'winter' : {}}
 
-    for season in seasons:
-        top_categories = list(Style.objects.filter(
-            season=season,
-            views__gte=10000
-        ).values('category').annotate(count=Count('category')).order_by('-count')[:5])
+#     for season in seasons:
+#         top_categories = list(Style.objects.filter(
+#             season=season,
+#             views__gte=10000
+#         ).values('category').annotate(count=Count('category')).order_by('-count')[:5])
 
-        other_count = Style.objects.filter(
-            season=season,
-            views__gte=10000
-        ).exclude(
-            category__in=[category['category'] for category in top_categories]
-        ).count()
+#         other_count = Style.objects.filter(
+#             season=season,
+#             views__gte=10000
+#         ).exclude(
+#             category__in=[category['category'] for category in top_categories]
+#         ).count()
 
-        response_data[season] = {
-            'top_categories': top_categories,
-            'other_count': other_count
-            }
+#         response_data[season] = {
+#             'top_categories': top_categories,
+#             'other_count': other_count
+#             }
 
-    logger.info(
-        f'********season_style_trend의 검색결과는 아래와 같습니다.\n'
-        f'season : {season}\n'
-        f'styles : {top_categories}\n'
-        f'other_count : {other_count}\n'
-    )
-    return response_data
+#     logger.info(
+#         f'********season_style_trend의 검색결과는 아래와 같습니다.\n'
+#         f'season : {season}\n'
+#         f'styles : {top_categories}\n'
+#         f'other_count : {other_count}\n'
+#     )
+#     return response_data
 
-def popular_styles_by_category(request):
-    """
-    카테고리에 대한 최상위 스타일 5개 조회
-    해당 스타일에 있는 사진, 그 스타일에 있는 상품 list 조회
-    """
-    category_name = request.GET.get('category')
-    if category_name:
-        top_styles = Style.objects.filter(category=category_name).order_by('-views')[:5]
+# def popular_styles_by_category(request):
+#     """
+#     카테고리에 대한 최상위 스타일 5개 조회
+#     해당 스타일에 있는 사진, 그 스타일에 있는 상품 list 조회
+#     """
+#     category_name = request.GET.get('category')
+#     if category_name:
+#         top_styles = Style.objects.filter(category=category_name).order_by('-views')[:5]
 
-        results = [
-            {
-                'subject': style.subject,
-                'views': style.views,
-                'url': style.URL,
-                'goods_list': list(StyleGoods.objects.filter(style=style).values('goods__name', 'goods__brand'))
-            } for style in top_styles
-        ]
-        logger.info(f'*******popular_styles_by_category : {results} 이 검색되었습니다.')
-        return JsonResponse(results, safe=False)
-    else:
-        return JsonResponse({'error': '카테고리가 지정되지 않았습니다.'}, status=400)
+#         results = [
+#             {
+#                 'subject': style.subject,
+#                 'views': style.views,
+#                 'url': style.URL,
+#                 'goods_list': list(StyleGoods.objects.filter(style=style).values('goods__name', 'goods__brand'))
+#             } for style in top_styles
+#         ]
+#         logger.info(f'*******popular_styles_by_category : {results} 이 검색되었습니다.')
+#         return JsonResponse(results, safe=False)
+#     else:
+#         return JsonResponse({'error': '카테고리가 지정되지 않았습니다.'}, status=400)
 
-def top_styles_by_season(request):
-    """
-    시즌에 대한 최상위 스타일 5개 조회
-    해당 스타일에 있는 사진, 그 스타일에 있는 상품 list 조회
-    """
-    season_name = request.GET.get('season')
-    if season_name:
-        top_styles = Style.objects.filter(season=season_name).order_by('-views')[:5]
+# def top_styles_by_season(request):
+#     """
+#     시즌에 대한 최상위 스타일 5개 조회
+#     해당 스타일에 있는 사진, 그 스타일에 있는 상품 list 조회
+#     """
+#     season_name = request.GET.get('season')
+#     if season_name:
+#         top_styles = Style.objects.filter(season=season_name).order_by('-views')[:5]
 
-        results = [
-            {
-                'subject': style.subject,
-                'views': style.views,
-                'url': style.URL,
-                'style_goods': [
-                    {'name': sg.goods.name, 'brand': sg.goods.brand}
-                    for sg in StyleGoods.objects.filter(style=style)
-                ]
-            } for style in top_styles
-        ]
-        logger.info(f'*******top_styles_by_season : {results} 이 검색되었습니다.')
-        return JsonResponse(results, safe=False)
-    else:
-        return JsonResponse({'error': 'need to choice season.'}, status=400)
+#         results = [
+#             {
+#                 'subject': style.subject,
+#                 'views': style.views,
+#                 'url': style.URL,
+#                 'style_goods': [
+#                     {'name': sg.goods.name, 'brand': sg.goods.brand}
+#                     for sg in StyleGoods.objects.filter(style=style)
+#                 ]
+#             } for style in top_styles
+#         ]
+#         logger.info(f'*******top_styles_by_season : {results} 이 검색되었습니다.')
+#         return JsonResponse(results, safe=False)
+#     else:
+#         return JsonResponse({'error': 'need to choice season.'}, status=400)
+
+#########################################       NEW      #################################################
+from django.shortcuts import render
+from .models import Style, StyleGoods, Goods
+from django.db.models import Count
+
+def top_styles_by_season(request, season):
+    styles = Style.objects.filter(season=season).annotate(num_views=Count('views')).order_by('-num_views')[:5]
+    context = {'styles': []}
+    for style in styles:
+        goods_list = StyleGoods.objects.filter(style=style).select_related('goods')[:3]
+        goods_data = [{
+            'name': sg.goods.name,
+            'brand': sg.goods.brand,
+            'price': sg.goods.discounted_price if sg.goods.discounted_price else sg.goods.origin_price
+        } for sg in goods_list]
+        context['styles'].append({'style': style, 'goods': goods_data})
+    return render(request, 'top_styles_by_season.html', context)
