5bca601238c4935ef0631e34e29ad512486709a8
hoon <jiroh618@gmail.com>
2023-11-17 15:59:47 +0900

    fix: restore outfit function

--- musinsa_trend/outfits/urls.py
+++ musinsa_trend/outfits/urls.py
@@ -9,23 +9,23 @@ from . import views
 # www.example.com:8000/static/media/image01.png
 
 urlpatterns = [
-    # path('index/', views.index, name='index'),
-    # path('chart', views.chart, name='chart'),
-    # path('styles_by_category', views.stylecat, name='styles_by_season'),
-    # path('styles_by_season', views.stylesea, name='styles_by_season'),
+    path('index/', views.index, name='index'),
+    path('chart', views.chart, name='chart'),
+    path('styles_by_category', views.stylecat, name='styles_by_season'),
+    path('styles_by_season', views.stylesea, name='styles_by_season'),
     
-    # # Generic schema view of app
-    # path('schema_view/', get_schema_view(
-    #     title="Musinsa Trend Analyzer",
-    #     description="All APIs",
-    #     version="1.0.0"
-    # ), name='schema_view'),
+    # Generic schema view of app
+    path('schema_view/', get_schema_view(
+        title="Musinsa Trend Analyzer",
+        description="All APIs",
+        version="1.0.0"
+    ), name='schema_view'),
 
-    # # Openapi schema view with UI
-    # path('openapi/', TemplateView.as_view(
-    #     template_name='openapi_schema.html',
-    #     extra_context={'schema_url':'schema_view'}
-    # ), name='openapi_schema_view'),
+    # Openapi schema view with UI
+    path('openapi/', TemplateView.as_view(
+        template_name='openapi_schema.html',
+        extra_context={'schema_url':'schema_view'}
+    ), name='openapi_schema_view'),
     # 기본 페이지 경로: 계절이 선택되지 않았을 때
     path('top-styles/', views.top_styles, name='top_styles'),
     # 계절이 선택되었을 때의 경로
