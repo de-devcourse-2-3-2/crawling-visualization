2e03e10d662101d6de6fbe8905d1dbbbb386bf83
Jaeho-99 <wogh6860@gmail.com>
2023-11-25 16:05:59 +0900

    fix: crawling fixed

--- crawling/main/main.py
+++ crawling/main/main.py
@@ -1,7 +1,7 @@
-from crawling.main.crawling import Crawling
+from crawling import Crawling
 
 # 여기서 컴퓨팅 리소스를 더 쓰되, 두 크롤링을 동시에 수행하게 만든다면 더욱 재밌
 if __name__ == '__main__':
     # DO create table
-    Crawling.main_page_crawling("https://www.musinsa.com/app/styles/lists")
-    Crawling.main_page_crawling("https://www.musinsa.com/app/codimap/lists")
+    Crawling(top_url="https://www.musinsa.com/app/styles/lists").main_page_crawling()
+    Crawling(top_url="https://www.musinsa.com/app/codimap/lists").main_page_crawling()
